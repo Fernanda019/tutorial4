f92e46391708977f720b43d07f0b6c2233936f02
Fernanda019 <fernanda05@gmail.com>
2026-05-29 11:54:28 -0500

    Configuracion del boton de eliminar

--- vue-libreria/src/views/LibrosView.vue
+++ vue-libreria/src/views/LibrosView.vue
@@ -1,11 +1,81 @@
 <script>
+import axios from 'axios';
+import { ref, onMounted } from 'vue';
+
+export default {
+    name: 'LibrosView',
+    setup() {
+        const libros = ref([]);
+
+        onMounted(() => {
+            axios.get('/api/libros')
+              .then(response => {
+                  console.log("RESPUESTA COMPLETA:", response.data);
+                  libros.value = response.data;
+              })
+                .catch(error => {
+                    console.error('Error al obtener los libros:', error);
+                });
+        });
+
+        const eliminarLibro = async (id, titulo) => {
+            const confirmDelete = window.confirm(
+                `¿Estás seguro de que deseas eliminar el libro '${titulo}'?`
+            );
+            if (confirmDelete) {
+                try {
+                    await axios.delete(`/api/libros/${id}`);
+                } catch (error) {
+                    console.error('Error al eliminar el libro:', error);
+                }
+            }
+
+            onMounted(()=>{
+                listarLibros();
+            })
+        };
+        return {
+            libros,
+            listarLibros,
+        };
+    }
+};
 
 </script>
 
 <template>
-    <h1>Inicio</h1>
+    <main>
+        <table>
+            <thead>
+                <tr>
+                    <th>id</th>
+                    <th>Titulo</th>
+                    <th>ISBN</th>
+                    <th>Genero</th>
+                    <th>Precio</th>
+                    <th>Disponibilidad</th>
+                    <th>Acciones</th>
+                </tr>
+            </thead>
+
+            <tbody>
+                <tr v-for="libro in libros" :key="libro.id">
+                    <td>{{ libro.id }}</td>
+                    <td>{{ libro.titulo }}</td>
+                    <td>{{ libro.ISBN }}</td>
+                    <td>{{ libro.genero }}</td>
+                    <td>{{ libro.precio }}</td>
+                    <td>{{ libro.disponibilidad }}</td>
+                    <div>
+                        <button @click="eliminarLibro(libro.id,libro.titulo)">
+                            Eliminar
+                        </button>
+                    </div>
+                </tr>
+            </tbody>
+        </table>
+    </main>
 </template>
 
 <style>
-
 </style>
