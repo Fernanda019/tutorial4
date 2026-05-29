42135ae40d30854628030d315f634317b6654036
Fernanda019 <fernanda05@gmail.com>
2026-05-29 14:28:10 -0500

    Estilo para la lista navbar

--- vue-libreria/src/App.vue
+++ vue-libreria/src/App.vue
@@ -5,9 +5,9 @@ import { RouterLink, RouterView } from 'vue-router'
 <template>
   <header>
     <div class="wrapper">
-      <nav>
-        <RouterLink to="/">Libros</RouterLink>
-        <RouterLink to="/nuevoLibro">Nuevo Libro</RouterLink>
+      <nav class="navbar">
+        <RouterLink class="link--navbar" to="/">Libros</RouterLink>
+        <RouterLink class="link--navbar" to="/nuevoLibro">Nuevo Libro</RouterLink>
       </nav>
     </div>
   </header>
@@ -16,4 +16,26 @@ import { RouterLink, RouterView } from 'vue-router'
 </template>
 
 <style scoped>
+
+  .navbar {
+    background-color: #000000;
+    margin: 10px auto;
+    width: 400px;
+    display: flex;
+    justify-content: space-around;
+    border-radius: 5px;
+  }
+
+  .link--navbar {
+    text-decoration: none;
+    padding: 10px;
+    font-weight: 700;
+    font-size: 1.3rem;
+    color:#000000;
+    transition: color 0.3s ease-in-out;
+  }
+
+  .link--navbar:hover {
+    color: #000000;
+  }
 </style>
